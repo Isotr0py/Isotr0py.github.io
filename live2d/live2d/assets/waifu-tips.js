39d3a93794d1932d7052fed7f4f7b7f791429c17
Re Entr0py <2037008807@qq.com>
2022-03-24 00:05:36 +0800

    Site updated: 2022-03-24 00:05:34

--- live2d/live2d/assets/waifu-tips.js
+++ live2d/live2d/assets/waifu-tips.js
@@ -82,7 +82,32 @@ $('.waifu-tool .fui-photo').click(function (){
     
     if (window.location.href == SiteIndexUrl) {      // 如果是主页
         var now = (new Date()).getHours();
-        if (now > 23 || now <= 5) { text="你是夜猫子呀？这么晚还不睡觉，明天起的来嘛" ; } else if (now> 5 && now <= 7) { text="早上好！一日之计在于晨，美好的一天就要开始了" ; } else if (now> 7 && now <= 11) { text="上午好！工作顺利嘛，不要久坐，多起来走动走动哦！" ; } else if (now> 11 && now <= 14) { text="中午了，工作了一个上午，现在是午餐时间！" ; } else if (now> 14 && now <= 17) { text="午后很容易犯困呢，今天的运动目标完成了吗？" ; } else if (now> 17 && now <= 19) { text="傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~" ; } else if (now> 19 && now <= 21) { text="晚上好，今天过得怎么样？" ; } else if (now> 21 && now <= 23) { text="已经这么晚了呀，早点休息吧，晚安~" ; } else if(document.referrer !="=" ''){ var referrer="document.createElement('a');" referrer.href="document.referrer;" domain="referrer.hostname.split('.')[1];" if (window.location.hostname="=" referrer.hostname) + document.title.split(' - ')[0] '』< span>';
+        if (now > 23 || now <= 5) {
+            text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛';
+        } else if (now > 5 && now <= 7) {
+            text = '早上好！一日之计在于晨，美好的一天就要开始了';
+        } else if (now > 7 && now <= 11) {
+            text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
+        } else if (now > 11 && now <= 14) {
+            text = '中午了，工作了一个上午，现在是午餐时间！';
+        } else if (now > 14 && now <= 17) {
+            text = '午后很容易犯困呢，今天的运动目标完成了吗？';
+        } else if (now > 17 && now <= 19) {
+            text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~';
+        } else if (now > 19 && now <= 21) {
+            text = '晚上好，今天过得怎么样？';
+        } else if (now > 21 && now <= 23) {
+            text = '已经这么晚了呀，早点休息吧，晚安~';
+        } else {
+            text = '嗨~ 快来逗我玩吧！';
+        }
+    } else {
+        if(document.referrer !== ''){
+            var referrer = document.createElement('a');
+            referrer.href = document.referrer;
+            var domain = referrer.hostname.split('.')[1];
+            if (window.location.hostname == referrer.hostname) {
+                text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
             } else if (domain == 'baidu') {
                 text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
             } else if (domain == 'so') {
@@ -197,4 +222,34 @@ function initModel(waifuPath,waifuNames,waifuTipsPath){
                     var after = tips.date.split('-')[0];
                     var before = tips.date.split('-')[1] || after;
                 
-                    if ((after.split('/')[0] </=></=></=></=></=></=></=></=>
+                    if ((after.split('/')[0] <= now.getMonth() + 1 && now.getMonth() + 1 <= before.split('/')[0]) &&
+                        (after.split('/')[1] <= now.getDate() && now.getDate() <= before.split('/')[1])) {
+                        var text = tips.text;
+                        if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+                        text = text.render({ year: now.getFullYear() });
+                        showMessage(text, 6000, true);
+                    }
+                });
+            }
+        });
+    }
+
+
+}
+
+function loadModel(resourcePath, modelNames){
+    live2dLoader.resourcesConfig.setResourcesPath(resourcePath)
+    live2dLoader.resourcesConfig.setModelNames(modelNames)
+    live2dLoader.start();
+}
+
+function loadRandModel() {
+    //随机加载mod
+    live2dLoader.changeScene(-1);
+}
+
+function loadOtherModel() {
+    
+    live2dLoader.changeScene(0);
+   
+}
